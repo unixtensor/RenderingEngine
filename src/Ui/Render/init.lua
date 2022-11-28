8099e59cf5a0c17991d022f7756cb367940a0ca2
interpreterK <rhpidfyre@tuta.io>
2022-11-28 16:36:33 -0500

    Create repository for a database

---
+++ src/Ui/Render/init.lua
@@ -0,0 +1,59 @@
+local Renderer = {}
+Renderer.__index = Renderer
+
+local max, abs, floor, min = math.max, math.abs, math.floor, math.min
+local insert = table.insert
+
+local function MakePixel(w, h, dpi)
+	local h, l = max(w+h)+.0, .0
+	while abs(h-l)>1e-6 do
+		local mid = (l+h)/2.0
+		local midval = floor(w/mid)*floor(h/mid)
+		if midval>=dpi then
+			l=mid
+		elseif midval<dpi then
+			h=mid
+		end
+	end
+	return min(w/floor(w/l), h/floor(h/l))
+end
+local function E_clamp(n1, n2, n3) --Error-less clamp
+	return max(n1, min(n2, n3))
+end
+
+function Renderer.new(Parent, COLOR, DPI, SCALE, DENSITY, SHIFT_FACTOR)
+	local self = {}
+	self.DPI = DPI or 10 --how many pixels (more = further/zoom, also more color space)
+	self.SCALE = SCALE or 30
+	self.DENSITY = DENSITY or .1 --how much detail
+	self.SHIFT_FACTOR = SHIFT_FACTOR or 5 --the size of 3D (can be dangerous depending on DPI,)
+	self.COLOR_LERP = SCALE/(DPI-SHIFT_FACTOR/2) --(lower = starts off darker)
+	return setmetatable(self, Renderer)
+end
+
+function Renderer:Cube()
+	local Size = {x = 2000, y = 2000}
+	local Pos = {x = 50, y = 100}
+	local Cal = floor((Size.y/(MakePixel(Size.x, Size.y, self.DPI))*self.SCALE)*self.SHIFT_FACTOR)
+	local Frames = {}
+
+	local Bin = Instance.new("Folder", self.Parent)
+	Bin.Name = "Bin"
+	for i = 1, Cal do
+		local c = E_clamp(0,i/self.COLOR_LERP,255)
+		local S = MakePixel(Size.x, Size.y, self.DPI)
+		local Frame = Instance.new("Frame", Bin)
+		Frame.BorderSizePixel = 0
+		Frame.Size = UDim2.fromOffset(S,S)
+		Frame.BackgroundColor3 = Color3.new(
+			self.COLOR.r~=0 and c/255 or 0,
+			self.COLOR.g~=0 and c/255 or 0,
+			self.COLOR.b~=0 and c/255 or 0
+		)
+		Frame.Position = UDim2.fromOffset(Pos.x+(S+i)*self.DENSITY,Pos.y+(S+i)*self.DENSITY)
+		insert(Frames, Frame)
+	end
+	return {Frames=Frames, Cal=Cal, COLOR_LERP=self.COLOR_LERP}
+end
+
+return Renderer
---
+++ src/Ui/init.client.lua
@@ -0,0 +1,131 @@
+local Screen = script.Parent:WaitForChild("Screen")
+local _2D_Render = script.Parent:WaitForChild("2D_Render")
+local _3D_Render = script.Parent:WaitForChild("3D_Render")
+local MakeCube = script.Parent:WaitForChild("MakeCube")
+local MakeSquare = script.Parent:WaitForChild("MakeSquare")
+local RemoveRender = script.Parent:WaitForChild("RemoveRender")
+local RenderRotate = script.Parent:WaitForChild("RenderRotate")
+local RenderRotateAnimate = script.Parent:WaitForChild("RenderRotateAnimate")
+local Frame = script.Parent:WaitForChild("Frame")
+local SpinRate = script.Parent:WaitForChild("SpinRate")
+local SpinDelta = script.Parent:WaitForChild("SpinDelta")
+local Shadows = script.Parent:WaitForChild("Shadows")
+local Brightness = script.Parent:WaitForChild("Brightness")
+local SpinDivision = script.Parent:WaitForChild("SpinDivision")
+local ColorField = script.Parent.Color
+
+local Render = require(script:WaitForChild("Render"))
+local Color = {r=255,g=0,b=0}
+local Spin = false
+local Spin_Rate = 0.01
+local Spin_Delta = 5
+local Spin_Division = 0
+local _Shadows = false
+local Dark = false
+local Frames, Cal, COLOR_LERP
+
+local max, min = math.max, math.min
+
+local function E_clamp(n1, n2, n3) --Error-less clamp
+	return max(n1, min(n2, n3))
+end
+
+local function ReColor()
+
+end
+
+_3D_Render.MouseButton1Click:Connect(function()
+	local Screen = Render(Screen.void, Color)
+	Frames = Screen.Frames
+	Cal = Screen.Cal
+	COLOR_LERP = Screen.COLOR_LERP
+end)
+ColorField.FocusLost:Connect(function()
+	local s = ColorField.Text:split(",")
+	if s then
+		Color.r = tonumber(s[1])
+		Color.g = tonumber(s[2])
+		Color.b = tonumber(s[3])
+	end
+end)
+SpinRate.FocusLost:Connect(function()
+	Spin_Rate = tonumber(SpinRate.Text) or Spin_Rate
+end)
+SpinDelta.FocusLost:Connect(function()
+	Spin_Delta = tonumber(SpinDelta.Text) or Spin_Delta
+end)
+Shadows.MouseButton1Click:Connect(function()
+	_Shadows = not _Shadows
+	if _Shadows then
+		for i = 1, Cal do
+			local c = E_clamp(0,i/COLOR_LERP,255)
+			for f = 1, #Frames do
+				Frames[f].BackgroundColor3 = Color3.new(
+					Color.r~=0 and c/255 or 0,
+					Color.g~=0 and c/255 or 0,
+					Color.b~=0 and c/255 or 0
+				)
+			end
+		end
+		Shadows.Text = '[Shadows]'
+	else
+		for i = 1, Cal do
+			local c = E_clamp(0,i/COLOR_LERP,255)
+			for f = 1, #Frames do
+				Frames[f].BackgroundColor3 = Color3.new(
+					Color.r~=0 and c/255 or 0,
+					Color.g~=0 and c/255 or 0,
+					Color.b~=0 and c/255 or 0
+				)
+			end
+		end
+		Shadows.Text = 'Shadows'
+	end
+end)
+RemoveRender.MouseButton1Click:Connect(function()
+	Screen.void.Bin:Destroy()
+	Frames = nil
+end)
+RenderRotate.MouseButton1Click:Connect(function()
+	for i = 1, #Frames do
+		Frame.Text = 'Frame: ' .. tostring(i)
+		for r = 1, 30 do
+			Frames[i].Rotation += r/Spin_Delta
+		end
+	end
+end)
+SpinDivision.FocusLost:Connect(function()
+	Spin_Division = tonumber(SpinDelta.Text) or Spin_Division
+end)
+RenderRotateAnimate.MouseButton1Click:Connect(function()
+	Spin = not Spin
+	if Spin then
+		RenderRotateAnimate.Text = '[Spin (Animated)]'
+		while true do
+			if not Spin then
+				RenderRotateAnimate.Text = 'Spin (Animated)'
+				break
+			end
+			for i = 1, #Frames do
+				Frame.Text = 'Frame: ' .. tostring(i)
+				for r = 1, 30 do
+					Frames[i].Rotation += r/Spin_Delta
+				end
+				if Spin_Division ~= 0 then
+					task.wait(Spin_Division/1e10)
+				end
+			end
+			task.wait(Spin_Rate)
+		end
+	end
+end)
+Brightness.MouseButton1Click:Connect(function()
+	Dark = not Dark
+	if Dark then
+		Screen.void.BackgroundColor3 = Color3.new(0,0,0)
+		Brightness.Text = 'Dark'
+	else
+		Screen.void.BackgroundColor3 = Color3.new(1,1,1)
+		Brightness.Text = 'Light'
+	end
+end)
